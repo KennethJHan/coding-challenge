cfaf25b743716150ad6e4dff58b8482290aaa29d
KennethJHan <kenneth.jh.han@gmail.com>
2023-10-14 09:07:08 +0900

    231014

---
+++ codeforces/4A-Watermelon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int w;
+    std::cin >> w;
+    if (w == 2)
+        std::cout << "NO" << std::endl;
+    else if (w % 2 == 0)
+        std::cout << "YES" << std::endl;
+    else
+        std::cout << "NO" << std::endl;
+    return 0;
+}
