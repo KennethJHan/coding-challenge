36a6c8941f96fab24ca73dae8d7bc35aee28e22d
KennethJHan <kenneth.jh.han@gmail.com>
2024-04-06 20:47:33 +0900

    240406

--- baekjoon_python/BronzeII/src/p_11944.py
+++ baekjoon_python/BronzeII/src/p_11944.py
@@ -1,4 +1,24 @@
-def make_nn(n: int, m: int):
+def make_nn(n: int, m: int) -> str:
+    """Return a string of n repeated n, truncated to length m.
+
+    Args:
+        n (int): number to repeat
+        m (int): max length of the string
+
+    Returns:
+        str: string of n repeated n, truncated to length m
+
+    Examples:
+        >>> res = make_nn(20, 16)
+        >>> print(res)
+        2020202020202020
+        >>> res = make_nn(1, 10)
+        >>> print(res)
+        1
+        >>> res = make_nn(5, 5)
+        >>> print(res)
+        55555
+    """
     res = ""
     for _ in range(n):
         res += str(n)
